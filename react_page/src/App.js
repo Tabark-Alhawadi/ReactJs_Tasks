a1113f38ed180f4323fe7801c91223d2609bdb58
TabarkAlhawadi <tabarkalhawadi1@gmail.com>
2023-02-07 22:35:34 +0300

    task one

---
+++ react_page/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import Body from './components/Body';
+import Footer from './components/Footer';
+
+
+function App() {
+  return (
+      <React.Fragment>
+        <Header />
+        <Body />
+        <Footer />
+      </React.Fragment>
+  );
+}
+
+export default App;
---
+++ react_page/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer(){
+    return(
+        <footer>
+        <p>This is made with Pyar 💜 by Eng.Tabark</p>
+        <a href="tabarkalhawadi1@gmail.com">tabarkalhawadi1@gmail.com</a>
+      </footer>
+        )
+};
+
+export default Footer;
